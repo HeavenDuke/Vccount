42f52b01c8f8f1683fabc0fa118e0894e10c0652
HeavenDuke <trashlhc@163.com>
2017-07-18 14:14:48 +0800

    完成个人信息编辑

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="page has-navbar" v-nav="{title: '修改密码', showBackButton: true}">
     <div class="page-content text-center">
-      <h2 class="padding">Nothing here.</h2>
+      <von-input type="password"
+                 :value.sync="password.p_old"
+                 placeholder="请输入原始密码"
+                 label="原密码"></von-input>
+      <von-input type="password"
+                 :value.sync="password.p_new"
+                 placeholder="请输入新密码"
+                 label="新密码"></von-input>
+      <von-input type="password"
+                 :value.sync="password.p_confirm"
+                 placeholder="请再次输入新设置的密码，两次输入应一致"
+                 label="确认密码"></von-input>
+      <div class="padding">
+        <md-button class="button button-positive button-block">提交</md-button>
+      </div>
+
     </div>
   </div>
 </template>
+<script>
+  export default {
+      data () {
+          return {
+              password: {
+                  p_old: "",
+                  p_new: "",
+                  p_confirm: ""
+              }
+          }
+      }
+  }
+</script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,18 @@
         <div class="page-content padding-top">
             <list>
                 <!--TODO: 个人信息摘要-->
-                <item><h1>233</h1></item>
+                <item>
+                    <div class="head-container">
+                        <img src="/static/favicon.ico"/>
+                    </div>
+                    <div class="profile-container">
+                        <div class="name">刘瀚诚</div>
+                        <div class="meta">18811409948</div>
+                    </div>
+                    <div class="profile-entry-container">
+                        <div class="icon ion-ios-arrow-right entry"> </div>
+                    </div>
+                </item>
             </list>
             <list v-for="menus in menuGroup">
                 <item v-for="menu in menus" class="item-icon-right" @click.native="this.$router.go(menu.link)">
@@ -14,7 +25,7 @@
                     <i class="icon ion-ios-arrow-right"></i>
                 </item>
             </list>
-            <div class="list-action">
+            <div class="padding-left padding-right">
                 <md-button class="button button-assertive button-block">登出</md-button>
             </div>
         </div>
@@ -25,22 +36,67 @@
         data () {
             return {
                 menuGroup: [[{
-                    text: "个人信息管理",
-                    link: "/user/edit"
-                }, {
                     text: "修改密码",
                     link: "/password/edit"
                 }], [{
                     text: "应用信息",
                     link: "/about"
+                }, {
+                    text: "设置",
+                    link: "/setting"
                 }]]
             }
         }
     }
 </script>
 <style>
-    .list-action {
-        margin-left: 10px;
-        margin-right: 10px;
+
+    .head-container {
+        height: 50px;
+        float: left;
+        vertical-align: middle;
+        display: inline;
     }
+
+    .head-container img {
+        height: 50px;
+        width: 50px;
+        border: none;
+    }
+
+    .profile-container {
+        height: 50px;
+        float: left;
+        vertical-align: middle;
+        display: inline;
+        margin-left: 5%;
+    }
+
+    .profile-entry-container {
+        height: 50px;
+        float: right;
+        display: inline;
+        vertical-align: middle;
+    }
+
+    .profile-entry-container .entry {
+        height: 50px;
+        margin-top: 10px;
+        font-size: 30px;
+        line-height: 30px;
+        color: silver;
+    }
+
+    .profile-container .name {
+        margin-top: 5px;
+        font-size: 16px;
+        line-height: 20px;
+    }
+
+    .profile-container .meta {
+        margin-top: 5px;
+        font-size: 14px;
+        color: silver;
+    }
+
 </style>
